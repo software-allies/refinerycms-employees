bd088525dc78c06561d899583ea6dcaf570a0128
Christian <christian@softwareallies.com>
2014-07-31 10:40:23 -0500

    removing employee type from required fields

--- app/models/refinery/employees/employee.rb
+++ app/models/refinery/employees/employee.rb
@@ -7,8 +7,7 @@ module Refinery
 
       acts_as_indexed :fields => [:first_name, :last_name, :employee_type, :position, :about]
 
-      validates :first_name, :presence => true, :uniqueness => true
-      validates :last_name,:picture_id, :employee_type, :position, :about , :presence => true
+      validates :first_name, :last_name, :picture_id, :position, :about , :presence => true
 
       belongs_to :picture, :class_name => '::Refinery::Image'
     end
